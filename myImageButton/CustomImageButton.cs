dfc74b802308d425cf0875c975c8a4e9f9a59e4a
ali suliman <alisulimanq@gmail.com>
2020-12-18 08:53:43 +0200

    first commit

---
+++ myImageButton/CustomImageButton.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace myImageButton
+{
+    public partial class CustomImageButton: PictureBox
+    {
+        public CustomImageButton()
+        {
+            InitializeComponent();
+        }
+
+        private Image Normal_Image;
+        private Image Hovered_Image;
+
+        public Image Image_Normal
+        {
+            get { return Normal_Image; }
+            set { Normal_Image = value; }
+        }
+
+        public Image Image_Hovered
+        {
+            get { return Hovered_Image; }
+            set { Hovered_Image = value; }
+        }
+
+        private void CustomImageButton_MouseHover(object sender, EventArgs e)
+        {
+            this.Image = Hovered_Image;
+        }
+
+        private void CustomImageButton_MouseLeave(object sender, EventArgs e)
+        {
+            this.Image = Normal_Image;
+        }
+    }
+}
---
+++ super design/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿
+namespace super_design
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.customImageButton3 = new myImageButton.CustomImageButton();
+            this.customImageButton2 = new myImageButton.CustomImageButton();
+            this.customImageButton1 = new myImageButton.CustomImageButton();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customImageButton3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customImageButton2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customImageButton1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.customImageButton3);
+            this.panel1.Controls.Add(this.customImageButton2);
+            this.panel1.Controls.Add(this.customImageButton1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(900, 150);
+            this.panel1.TabIndex = 0;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Font = new System.Drawing.Font("Humanst521 BT", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(100)))), ((int)(((byte)(0)))));
+            this.label6.Location = new System.Drawing.Point(69, 30);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(150, 29);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Super Design";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-44, -43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(173, 167);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(845, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 21);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "_";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(869, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 21);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "X";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Humanst521 BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(800, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 22);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Settings";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Humanst521 BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(345, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 22);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Add";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Humanst521 BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(237, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 22);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Apply";
+            // 
+            // customImageButton3
+            // 
+            this.customImageButton3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.customImageButton3.Image = ((System.Drawing.Image)(resources.GetObject("customImageButton3.Image")));
+            this.customImageButton3.Image_Hovered = ((System.Drawing.Image)(resources.GetObject("customImageButton3.Image_Hovered")));
+            this.customImageButton3.Image_Normal = ((System.Drawing.Image)(resources.GetObject("customImageButton3.Image_Normal")));
+            this.customImageButton3.Location = new System.Drawing.Point(810, 63);
+            this.customImageButton3.Name = "customImageButton3";
+            this.customImageButton3.Size = new System.Drawing.Size(48, 47);
+            this.customImageButton3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.customImageButton3.TabIndex = 2;
+            this.customImageButton3.TabStop = false;
+            // 
+            // customImageButton2
+            // 
+            this.customImageButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.customImageButton2.Image = ((System.Drawing.Image)(resources.GetObject("customImageButton2.Image")));
+            this.customImageButton2.Image_Hovered = ((System.Drawing.Image)(resources.GetObject("customImageButton2.Image_Hovered")));
+            this.customImageButton2.Image_Normal = ((System.Drawing.Image)(resources.GetObject("customImageButton2.Image_Normal")));
+            this.customImageButton2.Location = new System.Drawing.Point(241, 63);
+            this.customImageButton2.Name = "customImageButton2";
+            this.customImageButton2.Size = new System.Drawing.Size(46, 47);
+            this.customImageButton2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.customImageButton2.TabIndex = 1;
+            this.customImageButton2.TabStop = false;
+            // 
+            // customImageButton1
+            // 
+            this.customImageButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.customImageButton1.Image = ((System.Drawing.Image)(resources.GetObject("customImageButton1.Image")));
+            this.customImageButton1.Image_Hovered = ((System.Drawing.Image)(resources.GetObject("customImageButton1.Image_Hovered")));
+            this.customImageButton1.Image_Normal = ((System.Drawing.Image)(resources.GetObject("customImageButton1.Image_Normal")));
+            this.customImageButton1.Location = new System.Drawing.Point(342, 63);
+            this.customImageButton1.Name = "customImageButton1";
+            this.customImageButton1.Size = new System.Drawing.Size(48, 47);
+            this.customImageButton1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.customImageButton1.TabIndex = 0;
+            this.customImageButton1.TabStop = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 150);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(900, 397);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 547);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customImageButton3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customImageButton2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customImageButton1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private myImageButton.CustomImageButton customImageButton2;
+        private myImageButton.CustomImageButton customImageButton1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private myImageButton.CustomImageButton customImageButton3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
